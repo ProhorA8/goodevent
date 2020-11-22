71e88cf3ccbfc657dd4c594f1c61ed3b3e1c63e8
ProhorA8 <Procustom4@gmail.com>
2020-11-22 15:43:27 +0300

    Initial commit

---
+++ maps.js
@@ -0,0 +1,11 @@
+ymaps.ready(init);
+
+function init() {
+    var myMap = new ymaps.Map("map", {
+            center: [54.072176, 27.836473
+            ],
+            zoom: 14
+        }, {
+            searchControlProvider: 'yandex#search'
+        })
+}
